0e6fdefb65dbd12a86c210dae93e1005d20f88ad
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2021-02-23 22:57:32 +0300

    feat: GeneratorFactory method

--- src/app/core/services/generator.service.ts
+++ src/app/core/services/generator.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class GeneratorService {
 
   constructor() { }
+
+  GeneratorFactory(n: number): string {
+    const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+
+    for (var i = 0; i < n; i++) {
+      const randomPoz = Math.floor(Math.random() * charSet.length);
+      randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+
+    return randomString;
+  }
 }
